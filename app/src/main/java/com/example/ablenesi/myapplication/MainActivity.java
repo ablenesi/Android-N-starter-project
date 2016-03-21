7f431beb3b85e721c0db57b6dd6c7c16f14efd3f
Attila Blenesi <battila@halcyonmobile.com>
2016-03-21 17:45:27 +0200

    Add basic starter project.

---
+++ app/src/main/java/com/example/ablenesi/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ablenesi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.test).setOnClickListener((View v) -> Log.d(TAG, "onClick: Button was pressed!"));
+    }
+}
